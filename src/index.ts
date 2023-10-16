e125b7fdb62e22aecdf16863317c894b0cebd3d8
Ander Frank Jesús Carrasco Cordova <78995097+anderfrankdev@users.noreply.github.com>
2023-10-16 14:24:34 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import express from "express";
 import "dotenv/config";
 import dbConnection from "./database/config.js";
 
+const port = Number(process.env.PORT) || 8080
+
 await dbConnection();
 
 const server_params = {
